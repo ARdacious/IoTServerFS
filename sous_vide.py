f1e2fb0e108955580fa1f7702a1fdffd6ef2d903
Arrie Dacioius <ardaciousdev@gmail.com>
2019-12-11 00:01:04 +0100

    [sous vide based on physics]
    heat using physics formula
    - amount of water
    - power of heating element
    
    Signed-off-by: Arrie Dacioius <ardaciousdev@gmail.com>

--- sous_vide.py
+++ sous_vide.py
@@ -7,25 +7,94 @@ from time import sleep
 from time import time
 tempErrors = 0
 def getTemps(server):
-        url = "http://%s/temp" % server
-        try:
-            r = urlopen(url)
-            l = int(r.headers['Content-Length'].split(',')[0])
-            content = r.read(l)
-        except:
-            global tempErrors
-            tempErrors += 1
-            sleep(3)
-            r = urlopen(url)
-            l = int(r.headers['Content-Length'].split(',')[0])
-            content = r.read(l)
-        temps = loads(content)
-        return temps
+    _s = time()
+    url = "http://%s/temp" % server
+    try:
+        r = urlopen(url)
+        l = int(r.headers['Content-Length'].split(',')[0])
+        content = r.read(l)
+    except:
+        global tempErrors
+        tempErrors += 1
+        sleep(3)
+        r = urlopen(url)
+        l = int(r.headers['Content-Length'].split(',')[0])
+        content = r.read(l)
+    temps = loads(content)
+    _dt = time()-_s
+    #print("temp took", _dt)
+    return temps, _dt
+
+def getTemp(verbose=False):
+    temps, _ = getTemps('192.168.1.9')
+    t = temps[0]['temp0']
+    if verbose: print("temp", t)
+    return t
+
+def neededWattsH(dT, vol):
+    # 1,160 W per celcius per liter
+    return vol * 4 * dT / 3.412
+
+def neededTime(watthrs, p):
+    return watthrs/p*3600
+
+def Off():
+    _s = time()
+    system('~/git/pi/lights-off "Stekker B" > /dev/null')
+    _dt = time()-_s
+    print("off took", _dt)
+    return time()-_s
+def On():
+    _s = time()
+    system('~/git/pi/lights-on "Stekker B" > /dev/null')
+    _dt = time()-_s
+    print("on took", _dt)
+    return time()-_s
+
+def waitForPeak():
+    _s = time()
+    start = getTemp()
+    t = start
+    print('wait rise')
+    while t <= (start + 0.13):
+        t = getTemp()
+        print (t, start-t)
+    print('rise start took:', time()-_s)
+    _s = time()
+    print('wait peak')
+    maxT = t
+    while t >= (maxT - 0.13):
+        maxT = max(t, maxT)
+        t = getTemp()
+        print (t, maxT-t)
+    print('peak took: ', time()-_s)
+    # return peak temperature
+    return maxT
+    
+Off();
+
+volume = 0.75
+targetT = 65
+tAct = targetT
+while True:
+    t = getTemp(verbose=False)
+    watthrs = neededWattsH(targetT - t, volume)
+    dT = neededTime(watthrs, 1800)
+    print("temp:", t, "time needed:", dT, "error:", tAct-targetT)
+    if dT > 0.6:
+        print(watthrs, dT)
+        On()
+        sleep(dT)
+        Off()
+        Off()
+        tAct = waitForPeak()
+
+exit()
 
 print('start')
 sleep(1)
 print('ready')
-server = '192.168.1.19'
+server = '192.168.1.9'
 on = False
 ts = time()
 maxdt = 60*3
@@ -35,7 +104,7 @@ system('~/git/pi/lights-off "Stekker B" > /dev/null')
 system('~/git/pi/lights-off "Stekker B" > /dev/null')
 try:
     while True:
-        temps = getTemps(server)
+        temps, deltaT = getTemps(server)
         now = time()
         dt = now - ts
         #print("heating:", on)
@@ -55,14 +124,14 @@ try:
             ts = now
             maxdt = 1
             print("turn off", t, dt, maxdt)
-            system('~/git/pi/lights-off "Stekker B" > /dev/null')
-            system('~/git/pi/lights-off "Stekker B" > /dev/null')
+            Off()
+            Off()
             on = False
         elif t < targetT and not on and dt > minoff:
             ts = now
             print("turn on", t, dt, maxdt)
-            system('~/git/pi/lights-on "Stekker B" > /dev/null')
-            system('~/git/pi/lights-on "Stekker B" > /dev/null')
+            On()
+            On()
             on = True
         else:
             print("heating", on, t, dt, maxdt, minoff, tempErrors)
