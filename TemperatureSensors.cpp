b29b6c662b6a2e6f669927fe3704d519530132ce
Stephen Carter <carter040@gmail.com>
2019-12-12 00:05:54 +0100

    [replace by max6675 and PT100]
    Use temperature sensors for smoker
    max6675
    pt100 for in food
    
    Signed-off-by: Stephen Carter <carter040@gmail.com>

--- TemperatureSensors.cpp
+++ TemperatureSensors.cpp
@@ -1,102 +1,56 @@
+#include <SPI.h>
 
-#include <OneWire.h>
-#include <DallasTemperature.h>
-#include <Arduino.h>
+double readCelsius(uint8_t cs) {
+    uint16_t v;
 
-#include "P1Logger.h"
+    digitalWrite(cs, LOW);
+    v = SPI.transfer(0x00);
+    v <<= 8;
+    v |= SPI.transfer(0x00);
+    digitalWrite(cs, HIGH);
 
-// one wire on D1 / GPIO5
-#define ONE_WIRE_BUS  5
-
-// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
-OneWire oneWire(ONE_WIRE_BUS);
-// Pass our oneWire reference to Dallas Temperature. 
-DallasTemperature sensors(&oneWire);
-
-//DeviceAddress ambientAddr = {40, 97, 100, 18, 46, 126, 6, 12};
-
-void setupTempSensors()
-{
-  broadcast("Detect temperature sensors:");
-  sensors.begin();
-  int c = sensors.getDeviceCount();
-  int i,j;
-  DeviceAddress temp;
-  for (i = 0; i < c; i++) {
-    String msg = "";
-    msg += "Sensor ";
-    msg += String(i);
-    msg += ": ";
-    if (sensors.getAddress(temp, i)) {
-      for (j=0; j<8; j++) {
-        msg += String(temp[j]);
-        msg += ", ";
-      }
+    if (v & 0x4) {
+        // uh oh, no thermocouple attached!
+        Serial.println("No thermo attached");
+        return NAN; 
     }
-    broadcast(msg);
-  }
-}
-
-void reportTemperatures()
-{
-  broadcast("Report temperature sensors:");
-  int c = sensors.getDeviceCount();
-  int i;
-  float temp;
-  sensors.requestTemperatures();
-  for (i = 0; i < c; i++) {
-    String msg = "";
-    msg += "Sensor ";
-    msg += String(i);
-    msg += ": ";
-    temp = sensors.getTempCByIndex(i);
-    msg += String(temp);
-    msg += "C";
-    broadcast(msg);
-  }
-}
-
-void reportAllJSON() {
-  Serial.println("Report All JSON temperature sensors:");
-  int c = sensors.getDeviceCount();
-  int i;
-  float temp;
-  String json = "{";
-  json += "\"count\":" + String(c);
-  sensors.requestTemperatures();
-  for (i = 0; i < c; i++) {
-    temp = sensors.getTempCByIndex(i);
-    json += "\"temp" + String(i) + "\":" + String(temp) + ",";
-  }
-  json.remove(json.length()-1,1);
-  json += "}";  
-  broadcast(json);
-}
-
-
-float getTemp() {
-  return sensors.getTempCByIndex(0);
-}
-
-#include <ESP8266WebServer.h>
-
-extern ESP8266WebServer server;
-
-void handleTempList() {
-  Serial.println("Report All JSON temperature sensors:");
-  String output = "[";
-  int c = sensors.getDeviceCount();
-  int i;
-  float temp;
-  sensors.requestTemperatures();
-  for (i = 0; i < c; i++) {
-    temp = sensors.getTempCByIndex(i);
-    if (output != "[") {
-      output += ',';
+    else if (v == 0x0) {
+        Serial.println("No Max6675 detected");
+        return NAN;
     }
-    output += "{\"temp" + String(i) + "\":" + String(temp) + "}";
-  }
 
-  output += "]";
-  server.send(200, "text/json", output);
+    v >>= 3;
+
+    return v*0.25;
+}
+
+#define CS0 15
+#define CS1 16
+#define CS2  2
+
+void setup() {
+    SPI.begin();
+    pinMode(CS0, OUTPUT);
+    pinMode(CS1, OUTPUT);
+    pinMode(CS2, OUTPUT);
+    digitalWrite(CS0, HIGH);
+    digitalWrite(CS1, HIGH);
+    digitalWrite(CS2, HIGH);
+
+    Serial.begin(115200);
+    Serial.println("Hi...");
+}
+
+void loop() {
+    Serial.print(readCelsius(CS0));
+    Serial.print(" ");
+    Serial.print(readCelsius(CS1));
+    Serial.print(" ");
+    Serial.print(readCelsius(CS2));
+    Serial.print(" ");
+    float t = analogRead(A0);
+    t = (t - 360) * 1.454545455;
+    Serial.print(t);
+    Serial.println();
+    delay(1000);
 }
